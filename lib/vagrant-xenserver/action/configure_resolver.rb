094ab572c99ad67147e1992bbd05cec1916ef406
Sayid Munawar <sayid.munawar@gmail.com>
2016-04-21 23:09:51 +0700

    dont set /etc/resolv if dhcp is enabled

--- lib/vagrant-xenserver/action/configure_resolver.rb
+++ lib/vagrant-xenserver/action/configure_resolver.rb
@@ -15,25 +15,38 @@ module VagrantPlugins
 
         def call(env)
           # Accumulate the DNS configured in env[:vifs][:dns]
+          dhcp_enabled = false
           dns = Set.new
           env[:vifs].each do |vif, option|
+
+            # Skip if any interface is using DHCP
+            # Assume resolver is configured via DHCP
+            if !option[:proto].nil?
+              if option[:proto] == "dhcp"
+                dhcp_enabled = true
+                break
+              end
+            end
+
             if !option[:dns].nil?
               option[:dns].split(/[\s,;]+/).each do |ns|
                 dns.add(ns)
               end
             end
           end
-          dns_str = dns.to_a.join(", ")
-          @logger.info("Configuring DNS [#{dns_str}]")
-          env[:ui].info I18n.t("vagrant_xenserver.info.configure_resolver",
-            dns: dns_str)
 
-          env[:machine].guest.capability(
-            :configure_resolver, dns)
+          if not dhcp_enabled
+            dns_str = dns.to_a.join(", ")
+            @logger.info("Configuring DNS [#{dns_str}]")
+            env[:ui].info I18n.t("vagrant_xenserver.info.configure_resolver",
+              dns: dns_str)
+
+            env[:machine].guest.capability(
+              :configure_resolver, dns)
+          end
 
           # finally
           @app.call env
-
         end
       end
     end
